1901255422b9ae068c1f043401fc1b52229f0731
DouglasBarbino <DouglasBarbino@users.noreply.github.com>
2015-08-24 19:31:58 -0300

    Create EntradaTabelaDeSimbolos.java
    Arquivo DO PROFESSOR para inserção dos tokens na tabela de simbolos. Não foi modificado nada em relação a versão original e precisa ser entendida melhor (pelo menos eu preciso!)

---
+++ EntradaTabelaDeSimbolos.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho1;
+
+/**
+ *
+ * @author Usuário
+ */
+public class EntradaTabelaDeSimbolos {
+    private String nome, tipo;
+    
+    public EntradaTabelaDeSimbolos(String nome, String tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public String getTipo() {
+        return tipo;
+    }
+    
+    @Override
+    public String toString() {
+        return nome+"("+tipo+")";
+    }
+}
