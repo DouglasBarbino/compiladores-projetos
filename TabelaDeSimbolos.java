3e0da75187d58d270f6cdc88742af67c8dc8c539
BrenoSS <BrenoSS@users.noreply.github.com>
2015-09-25 14:54:55 -0300

    Update TabelaDeSimbolos.java

--- TabelaDeSimbolos.java
+++ TabelaDeSimbolos.java
@@ -29,16 +29,47 @@ public class TabelaDeSimbolos {
         this.escopo = escopo;
     }
     
-    public void adicionarSimbolo(String nome, String tipo, List<String> parametros) {
-        simbolos.add(new EntradaTabelaDeSimbolos(nome,tipo, parametros));
+    public void adicionarSimbolo(String nome, String tipo, List<String> parametros, TabelaDeSimbolos sub) {
+        simbolos.add(new EntradaTabelaDeSimbolos(nome,tipo, parametros, sub));
     }
     
-    public void adicionarSimbolos(List<String> nomes, String tipo, List<String> parametros) {
+    public void adicionarSimbolos(List<String> nomes, String tipo, List<String> parametros, TabelaDeSimbolos sub) {
         for(String s:nomes) {
-            simbolos.add(new EntradaTabelaDeSimbolos(s, tipo, parametros));
+            simbolos.add(new EntradaTabelaDeSimbolos(s, tipo, parametros, sub));
         }
     }
     
+    public String getTipo(String nome)
+    {
+        String tipo = null;
+        for(int i = 0; i < simbolos.size(); i++) {
+            if(simbolos.get(i).getNome().equals(nome)) {
+                tipo = simbolos.get(i).getTipo();
+            }
+        }
+        return tipo;
+    }
+    
+    public TabelaDeSimbolos getSubtabela(String tipo)
+    {
+        for(int i = 0; i < simbolos.size(); i++) {
+            if(simbolos.get(i).getNome().equals(tipo)) {
+                return simbolos.get(i).getsubTabela();
+            }
+        }
+        
+        return null;
+    }
+    
+//    public TabelaDeSimbolos getTabela(String tipo)
+//    {
+//        for(EntradaTabelaDeSimbolos etds:simbolos) {
+//            if(etds.getNome().equals(tipo)) {
+//                return true;
+//            }
+//        }
+//    }
+        
     public boolean existeSimbolo(String nome) {
         for(EntradaTabelaDeSimbolos etds:simbolos) {
             if(etds.getNome().equals(nome)) {
