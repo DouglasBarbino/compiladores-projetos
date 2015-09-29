ac92027cf6ef47dbfc65e01c1bb6ec6cdda78be4
DouglasBarbino <DouglasBarbino@users.noreply.github.com>
2015-09-29 19:33:21 -0300

    Update GeradorCodigo.java

--- GeradorCodigo.java
+++ GeradorCodigo.java
@@ -23,6 +23,7 @@ public class GeradorCodigo extends LABaseListener {
         this.codigo = codigo;
     }
     
+    @Override
     public void enterPrograma(LAParser.ProgramaContext ctx) {
         //Importacoes iniciais e mais uma linha em branco que eh pulada
         codigo.println("#include <stdio.h>");
@@ -30,28 +31,36 @@ public class GeradorCodigo extends LABaseListener {
         codigo.println("");
     }
     
+    @Override
     public void enterCorpo(LAParser.CorpoContext ctx){
         //Tudo o que eh declarado na funcao main
         codigo.println("int main() {");
     }
     
+    @Override
     public void exitCorpo(LAParser.CorpoContext ctx){
-        //Declarou tudo o que tinha pra se declarar na main(), fecha a chave dela
+        //Declarou tudo o que tinha pra se declarar na main(), retorna 0 e fecha a chave dela
+        codigo.println("return 0;");
         codigo.println("}");
     }
     
+    @Override
     public void enterDeclaracao_local(LAParser.Declaracao_localContext ctx) {
-        
         // declarando um tipo de registro
         if (ctx.getStart().getText().equals("tipo")){
             codigo.println("typedef struct {");
         }
     }
     
+    @Override
     public void enterVariavel(LAParser.VariavelContext ctx) {
         //Declarando uma variavel
         String nome = ctx.IDENT().getText();
         String tipo = null;
+        
+        // declaracao de um registro
+        if (ctx.tipo().getStart().getText().equals("registro"))
+            codigo.println("struct {");
         
         //Caso foi declarada mais de uma variavel com esse tipo
         //deixar desse jeito para montar o codigo, para adicionar na tabela de simbolos utilizar a regra do mais_var
@@ -87,6 +96,7 @@ public class GeradorCodigo extends LABaseListener {
         codigo.println(tipo+" "+nome+";");
     }
     
+    @Override
     public void exitRegistro(LAParser.RegistroContext ctx){
         
         String variaveis;
@@ -108,9 +118,10 @@ public class GeradorCodigo extends LABaseListener {
         codigo.println("} "+variaveis);
     }
     
+    @Override
     public void enterCmd(LAParser.CmdContext ctx){
         
-        String variavel, expressao = " = ";
+        String variavel, expressao = " = ", condicao, estrutura, tratamentoOpRelacional;
         
         //onde irao ser feitas as equacoes
         //atribuicao simples
@@ -135,7 +146,83 @@ public class GeradorCodigo extends LABaseListener {
             
             codigo.println(variavel+expressao+";");
         }
+        else{
+            
+            estrutura = ctx.getStart().getText();
+            
+            // verifica se eh se, caso, para ou enquanto pois sua estrutura inicial eh parecida
+            if (estrutura.equals("se") || estrutura.equals("caso") || 
+                estrutura.equals("para") || estrutura.equals("enquanto")){
+                
+                switch (estrutura){
+                    case "se":
+                        condicao = "if";
+                        break;
+                    case "caso":
+                        condicao = "switch";
+                        break;
+                    case "para":
+                        condicao = "for";
+                        break;
+                    default: //enquanto
+                        condicao = "while";
+                }
+                //adiciona parenteses para todos
+                condicao = condicao + " (";
+                
+                //preenchimento do parenteses
+                switch (estrutura){
+                    case "se":
+                    case "enquanto":
+                        //pega a primeira parte de uma expressao
+                        condicao = ctx.expressao().termo_logico().fator_logico().parcela_logica().exp_relacional().exp_aritmetica().getText();
+                        //verifica se op_relacional nao eh vazio pois dois dos operadores precisam ser convertidos para C
+                        if (ctx.expressao().termo_logico().fator_logico().parcela_logica().exp_relacional().op_opcional().op_relacional() != null){
+                            condicao = condicao + tratamentoOpRelacional(ctx);
+                        }
+                        else
+                            condicao = condicao + ctx.expressao().termo_logico().fator_logico().parcela_logica().exp_relacional().op_opcional().getText();
+                        
+                        break;
+                    case "caso":
+                        condicao = "switch";
+                        break;
+                    case "para":
+                        condicao = "for";
+                        break;
+                    default: //enquanto
+                        condicao = "while";
+                }
+                
+                //fecha o parenteses para todos e imprime no codigo
+                condicao = condicao + ") {";
+                codigo.println(condicao);
+            }
+        }
         //escrita
-        
+       
+    }
+    
+    private String tratamentoOpE(LAParser.CmdContext ctx) {
+        //nao esta pronto
+        return "&&";
+    }
+    
+    private String tratamentoOpOU(LAParser.CmdContext ctx) {
+        //nao esta pronto
+        return "||";
+    }
+    
+    private String tratamentoOpRelacional(LAParser.CmdContext ctx) {
+        String tratamentoOp = ctx.expressao().termo_logico().fator_logico().parcela_logica().exp_relacional().op_opcional().op_relacional().getText();
+        if (tratamentoOp.equals("="))
+            tratamentoOp = "==";
+        else{
+            if (tratamentoOp.equals("<>"))
+                tratamentoOp = "!=";
+        }
+        tratamentoOp = tratamentoOp + ctx.expressao().termo_logico().fator_logico().parcela_logica().exp_relacional().op_opcional().exp_aritmetica().getText();
+
+        return tratamentoOp;
     }
 }
