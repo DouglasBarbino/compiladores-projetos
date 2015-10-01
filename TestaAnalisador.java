99e3f99cf43a4de59ce20973beda4845b11e2db4
DouglasBarbino <DouglasBarbino@users.noreply.github.com>
2015-10-01 19:47:08 -0300

    Update TestaAnalisador.java
    Incluindo a verificação da geração de código

--- TestaAnalisador.java
+++ TestaAnalisador.java
@@ -13,23 +13,27 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker;
 public class TestaAnalisador {
 
     public static void main(String args[]) throws IOException, RecognitionException 
-  {
-        Saida out = new Saida();
+    {
     // Descomente as linhas abaixo para testar o analisador gerado.
     // Obs: a linha abaixo está configurada para usar como entrada o arquivo lua1.txt
     // Modifique-a para testar os demais exemplos
 
+        //para verificar se vai executar a geracao de codigo ou nao
+        String saida = null;
+      
+        Saida out = new Saida();
+        Saida codigo = new Saida();
         
         ANTLRInputStream input = new ANTLRInputStream(new FileInputStream(args[0]));
         
-       // ANTLRInputStream input = new ANTLRInputStream(new FileInputStream("casosDeTesteT1/1.arquivos_com_erros_sintaticos/entrada/2-algoritmo_2-2_apostila_LA_1_erro_linha_15.txt"));
+        //ANTLRInputStream input = new ANTLRInputStream(new FileInputStream("E:\\Facul\\6 sem\\CC2\\T1\\casosDeTesteT1\\2.arquivos_com_erros_semanticos\\entrada\\17.algoritmo_9-2_apostila_LA.txt"));
+        //ANTLRInputStream input = new ANTLRInputStream(new FileInputStream("E:\\Facul\\6 sem\\CC2\\T1\\casosDeTesteT1\\3.arquivos_sem_erros\\1.entrada\\10.caso_impressao.alg"));
         LALexer lexer = new LALexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         LAParser parser = new LAParser(tokens);
         // artificio utilizado para que o parser só execute o primeiro erro e pare a execução
         try 
         {
-
             parser.addErrorListener(new T1ErrorListener(out));
             LAParser.ProgramaContext raiz = parser.programa();
             AnalisadorSemantico as = new AnalisadorSemantico(out);
@@ -37,12 +41,46 @@ public class TestaAnalisador {
             ParseTreeWalker ptw = new ParseTreeWalker();
             ptw.walk(as, raiz);
             
+            //pega toda a saida que o out produziu
+            saida = out.getTexto();
         } 
         catch(ParseCancellationException pce) 
         {
-        
             if(pce.getMessage() != null)
                 out.println(pce.getMessage());
+        }
+        
+        //caso nao teve erros o tamanho de saida serah 0
+        if (saida.length() == 0){
+            
+            //Eh necessario redeclarar tudo outra vez...
+            ANTLRInputStream inputGC = new ANTLRInputStream(new FileInputStream(args[0]));
+            //ANTLRInputStream inputGC = new ANTLRInputStream(new FileInputStream("E:\\Facul\\6 sem\\CC2\\T1\\casosDeTesteT1\\3.arquivos_sem_erros\\1.entrada\\10.caso_impressao.alg"));
+            LALexer lexerGC = new LALexer(inputGC);
+            CommonTokenStream tokensGC = new CommonTokenStream(lexerGC);
+            parser.addErrorListener(new T1ErrorListener(codigo));
+            LAParser parserGC = new LAParser(tokensGC);
+            LAParser.ProgramaContext raizGC = parserGC.programa();
+            GeradorCodigo gc = new GeradorCodigo(codigo);
+            
+            ParseTreeWalker ptwGC = new ParseTreeWalker();
+            ptwGC.walk(gc, raizGC);
+         
+            PrintWriter pwGC = new PrintWriter(new File(args[1]));
+            //PrintWriter pwGC = new PrintWriter(new File("saidaGC.txt"));
+            pwGC.print(codigo.getTexto());
+            pwGC.flush();
+            pwGC.close();
+        }
+        //nao eh a geracao de codigos, vai imprimir os erros
+        else{
+            out.println("Fim da compilacao");
+            
+            PrintWriter pw = new PrintWriter(new File(args[1]));
+            //PrintWriter pw = new PrintWriter(new File("saida.txt"));
+            pw.print(out.getTexto());
+            pw.flush();
+            pw.close();
         }
 //        if (!out.isModificado()) {
 //            out.println("Fim da analise. Sem erros sintaticos.");
@@ -52,13 +90,7 @@ public class TestaAnalisador {
 //            System.out.print(out);
 //        } else {
         
-        out.println("Fim da compilacao");
-//        }
         
-        PrintWriter pw = new PrintWriter(new File(args[1]));
-       // PrintWriter pw = new PrintWriter(new File("saida.txt"));
-        pw.print(out.getTexto());
-        pw.flush();
-        pw.close();
+//        }
     }
 }
